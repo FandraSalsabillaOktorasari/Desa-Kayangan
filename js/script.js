ad885799ef1af94abfdc2a8a1832d85fd5e01e50
Fandra Salsabilla Oktorasari <fandrakuliah@gmail.com>
2025-01-25 23:25:38 +0700

    final

--- js/script.js
+++ js/script.js
@@ -8,24 +8,3 @@ navbarBrand.addEventListener('click', function() {
         navbarBrand.classList.remove('clicked');
     }, 1000); // 1 detik
 });
-
-// Ambil elemen tombol
-const backToTopBtn = document.getElementById("backToTop");
-
-// Fungsi untuk menampilkan tombol saat scroll melewati section hero
-window.onscroll = function () {
-    const heroHeight = document.querySelector(".hero-section").offsetHeight;
-    if (window.scrollY > heroHeight) {
-        backToTopBtn.style.display = "flex"; // Tampilkan tombol
-    } else {
-        backToTopBtn.style.display = "none"; // Sembunyikan tombol
-    }
-};
-
-// Fungsi untuk scroll kembali ke atas
-backToTopBtn.onclick = function () {
-    window.scrollTo({
-        top: 0,
-        behavior: "smooth", // Animasi smooth scroll
-    });
-};
